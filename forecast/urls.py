4ba9d4721f411febb72a7884acaa7ece4cd3f032
ValikVasilkovskiy <valik.vasilkovskiy@gmail.com>
2018-10-19 11:35:28 +0300

    add templates and view functions

---
+++ forecast/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.weather_list, name='weather_list' )
+]
--- forecast/views.py
+++ forecast/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def weather_list(request):
+    return render(request, 'forecast/weather_list.html', {})
