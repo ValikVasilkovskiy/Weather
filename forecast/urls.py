92ba083ec9fed4deec542a772fe9a984b99151fb
ValikVasilkovskiy <valik.vasilkovskiy@gmail.com>
2018-10-26 17:34:52 +0300

    add templates: base and day_detail

--- forecast/urls.py
+++ forecast/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.weather_list, name='weather_list')
+    url(r'^$', views.weather_list, name='weather_list'),
+    url(r'^day/(?P<pk>\d+)/$', views.day_detail, name='day_detail'),
 ]
--- forecast/views.py
+++ forecast/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import DayWeatherForecast
+
+import calendar
+from datetime import datetime
 
 # Create your views here.
 
 def weather_list(request):
     forecasts = DayWeatherForecast.objects.all()
-    return render(request, 'forecast/weather_list.html', {"forecasts": forecasts})
+    return render(request, 'forecast/weather_list.html', {
+        "forecasts": forecasts,})
+
+def day_detail(request, pk):
+    day = get_object_or_404(DayWeatherForecast, pk=pk)
+    return render(request, 'forecast/day_detail.html', {'day': day})
